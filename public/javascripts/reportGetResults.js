226c4b2a1df9c47f05c77ba0509367c1a84b3f22
Adam Cowart <adam.cowart@headspring.com>
2018-09-01 14:09:53 -0500

    results

--- public/javascripts/reportGetResults.js
+++ public/javascripts/reportGetResults.js
@@ -7913,7 +7913,7 @@ window.addEventListener("load", afterLoad)
 function afterLoad() {
 // call our hosted API - maybe this should be localhost
 console.log("making api call")
-fetch('http://localhost:3000/reportGetResults')
+fetch('http://104.238.124.110:3000/reportGetResults')
     .then(function(res) {
         return res.json();
     }).then(function(json) {
